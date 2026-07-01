a66e78eca41f216a61735946d683c6d9a96dd71c
Arnav Tripathy <arnavt09>
2026-06-30 22:02:58 -0400

    Add RSVP model

---
+++ backend/models/RSVP.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const rsvpSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true  
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true
+    },
+    event: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true  
+    },
+    status: {
+      type: String,
+      default: "going"
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("RSVP", rsvpSchema);
