dfd8944063f90698e0ed7aad9a94682ff30c0414
Arnav Tripathy <arnavt09>
2026-06-30 22:34:11 -0400

    Add Event model

---
+++ backend/models/Event.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+
+const eventSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true  
+    },
+    eventDate: {
+      type: String,
+      required: true  
+    },
+    eventTime: {
+      type: String,
+      required: true  
+    },
+    club: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Club",
+      required: true  
+    },
+    attendees: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User"
+      }  
+    ]
+  },
+  {
+    timestamps: true
+  }  
+);
+
+module.exports = mongoose.model("Event", eventSchema);
